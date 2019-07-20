0d6e55d8735ae632c4c22002e90aebab90644782
Burak Koc <burakkayakoc@gmail.com>
2019-07-20 15:19:03 -0500

    Console shows the all animals on the list

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,24 +4,40 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Ostrich");
+            Console.WriteLine("3. Goat");
+            Console.WriteLine("4. Pig");
+            Console.WriteLine("5. Sheep");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
                 case 1:
                     ChooseGrazingField.CollectInput(farm, new Cow());
                     break;
-                default:
+                case 2:
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    break;
+                case 3:
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
                     break;
             }
         }
