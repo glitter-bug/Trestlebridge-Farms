0aa424dbd9ec8c75764bd4c7bfd73e8e3621a5d0
Burak Koc <burakkayakoc@gmail.com>
2019-07-21 13:24:51 -0500

    chicken house added, generc Interface is added, Console works; chicken house can be added

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,31 +3,32 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions
-{
-    public class CreateFacility
-    {
-        public static void CollectInput(Farm farm)
-        {
-            Console.WriteLine("1. Grazing field");
-            Console.WriteLine("2. Chicken House");
+namespace Trestlebridge.Actions {
+    public class CreateFacility {
+        public static void CollectInput (Farm farm) {
+            Console.WriteLine ("1. Grazing field");
+            Console.WriteLine ("2. Chicken House");
 
-            Console.WriteLine();
-            Console.WriteLine("Choose what you want to create");
+            Console.WriteLine ();
+            Console.WriteLine ("Choose what you want to create");
 
-            Console.Write("> ");
-            string input = Console.ReadLine();
+            Console.Write ("> ");
+            string input = Console.ReadLine ();
 
-            switch (Int32.Parse(input))
-            {
+            switch (Int32.Parse (input)) {
                 case 1:
-                    farm.AddGrazingField(new GrazingField());
-                    Console.WriteLine("You created a new Grazing farm!");
-                    Console.WriteLine("Press Enter to go to the main Menu");
-                    Console.ReadLine();
+                    farm.AddGrazingField (new GrazingField ());
+                    Console.WriteLine ("You created a new Grazing farm!");
+                    Console.WriteLine ("Press Enter to go to the main Menu");
+                    Console.ReadLine ();
                     break;
-                default:
+                case 2:
+                    farm.AddChickenHouse (new ChickenHouse ());
+                    Console.WriteLine ("You created a new Chicken House!");
+                    Console.WriteLine ("Press Enter to go to the main Menu");
+                    Console.ReadLine ();
                     break;
+
             }
         }
     }
