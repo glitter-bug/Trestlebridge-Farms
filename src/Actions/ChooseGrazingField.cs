d4d708b243000a541319e341758f16c6c8c756e2
Burak Koc <burakkayakoc@gmail.com>
2019-07-22 14:50:14 -0500

    add animal works

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -4,29 +4,24 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions
-{
-    public class ChooseGrazingField
-    {
-        public static void CollectInput(Farm farm, IGrazing animal)
-        {
-            Console.Clear();
+namespace Trestlebridge.Actions {
+    public class ChooseGrazingField {
+        public static void CollectInput (Farm farm, IGrazing animal) {
+            Console.Clear ();
 
-            for (int i = 0; i < farm.GrazingFields.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. Grazing Field");
+            for (int i = 1; i < farm.GrazingFields.Count; i++) {
+                Console.WriteLine ($"{i + 1}. Grazing Field");
             }
 
-            Console.WriteLine();
+            Console.WriteLine ();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine($"Place the {animal.Type}(s) where?");
+            Console.WriteLine ($"Place the {animal.Type}(s) where?");
 
-            Console.Write("> ");
-            int choice = Int32.Parse(Console.ReadLine());
+            Console.Write ("> ");
+            int choice = Int32.Parse (Console.ReadLine ()) - 1;
 
-
-            //farm.GrazingFields[choice].AddResource(animal);
+            farm.GrazingFields[choice].AddResource (animal);
 
             /*
                 Couldn't get this to work. Can you?
